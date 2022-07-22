379cad5e5740569aa7bbd96e481d3cba5abcd0d2
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-07-22 09:46:09 -0500

    initial commit

---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,15 @@
+import { EmployeeViews } from "./EmployeeViews"
+import { CustomerViews } from "./CustomerViews"
+
+export const ApplicationViews = () => {
+	
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    if (honeyUserObject.staff) {
+        return <EmployeeViews />
+    }
+    else {
+        return <CustomerViews />
+    }
+}
